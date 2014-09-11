41cbb86a331573c1a884c4322dbd504071b3e192
Andrew Burgess <andrew.burgess@embecosm.com>
2014-09-11 14:10:03 +0100

    Improved preservation of POINT after ispell-word.

--- home/DOT_emacs.d/my-elisp/andrew-ispell.el
+++ home/DOT_emacs.d/my-elisp/andrew-ispell.el
@@ -4,23 +4,24 @@
 (defadvice ispell-word (around apb-ispell-word)
   "Highlight correct spelling"
   (require 'pulse)
-  (let ((pos (point))
-        (word (ispell-get-word following))
-        start end)
-    (goto-char pos)
-    ;; De-structure return word info list.
-    (setq start (car (cdr word))
-          end (car (cdr (cdr word)))
-          word (car word))
-      
-      (if (eq ad-do-it nil)
-          ;; Hide the cursor while we flash the word, stops annoying cursor
-          ;; flicker.
-          (let ((vis (internal-show-cursor-p))
-                (win (get-buffer-window (current-buffer))))
-            (internal-show-cursor win nil)
-            (pulse-momentary-highlight-region start end 'ispell-correct-face)
-            (internal-show-cursor win vis)))))
+  (let* ((pos (point))
+         (word (ispell-get-word following))
+         ;; De-structure return word info list.
+         (start (car (cdr word)))
+         (end (car (cdr (cdr word))))
+         (word (car word))
+         (offset (- pos end)))
+    ;; Now perform the actual ispell-work call.
+    (let ((ispell-result ad-do-it))
+      ;; Reposition cursor.
+      (forward-char offset)
+      (unless ispell-result
+        ;; Hide the cursor while we flash the word, stops annoying cursor
+        ;; flicker.
+        (let ((vis (internal-show-cursor-p))
+              (win (get-buffer-window (current-buffer))))
+          (internal-show-cursor win nil)
+          (pulse-momentary-highlight-region start end 'ispell-correct-face)
+          (internal-show-cursor win vis))))))
 
-  
 (provide 'andrew-ispell)

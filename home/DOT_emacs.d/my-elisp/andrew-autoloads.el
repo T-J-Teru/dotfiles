49a3efb0304689bfda3d380055ed05ee53f96ab0
Andrew Burgess <andrew.burgess@embecosm.com>
2016-05-23 08:53:59 +0100

    emacs: Add autoloads file to emacs configuration
    This adds a newly generated autoloads file into my emacs configuration
    directory, and arranges for it to be loaded during emacs startup.

---
+++ home/DOT_emacs.d/my-elisp/andrew-autoloads.el
@@ -0,0 +1,119 @@
+;;; andrew-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "andrew-after-save" "andrew-after-save.el"
+;;;;;;  (22320 20658 745594 42000))
+;;; Generated autoloads from andrew-after-save.el
+
+(autoload 'andrew/make-executable-after-save "andrew-after-save" "\
+Called when a file is saved.  Makes the file executable if it
+looks like a script.
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil "andrew-cc-mode" "andrew-cc-mode.el" (21486
+;;;;;;  28112 437976 422000))
+;;; Generated autoloads from andrew-cc-mode.el
+
+(autoload 'andrew-cc-mode "andrew-cc-mode" "\
+
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil "andrew-cperl-mode" "andrew-cperl-mode.el"
+;;;;;;  (21486 28112 438976 387000))
+;;; Generated autoloads from andrew-cperl-mode.el
+
+(autoload 'andrew-cperl-mode "andrew-cperl-mode" "\
+
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil "andrew-diff-mode" "andrew-diff-mode.el" (22329
+;;;;;;  33443 535227 637000))
+;;; Generated autoloads from andrew-diff-mode.el
+
+(autoload 'andrew-diff-mode "andrew-diff-mode" "\
+
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil "andrew-forth-mode" "andrew-forth-mode.el"
+;;;;;;  (22329 33262 247639 31000))
+;;; Generated autoloads from andrew-forth-mode.el
+
+(autoload 'andrew-forth-mode "andrew-forth-mode" "\
+
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil "andrew-javascript-mode" "andrew-javascript-mode.el"
+;;;;;;  (22329 33376 840954 232000))
+;;; Generated autoloads from andrew-javascript-mode.el
+
+(autoload 'andrew-javascript-mode "andrew-javascript-mode" "\
+
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil "andrew-latex-mode" "andrew-latex-mode.el"
+;;;;;;  (22329 32779 443234 952000))
+;;; Generated autoloads from andrew-latex-mode.el
+
+(autoload 'andrew-latex-mode "andrew-latex-mode" "\
+
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil "menu-toggle" "menu-toggle.el" (21486 28112
+;;;;;;  437976 422000))
+;;; Generated autoloads from menu-toggle.el
+
+(autoload 'toggle-menubar-and-toolbar "menu-toggle" "\
+Switches off both menu and tool bar.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "mparen" "mparen.el" (21486 28112 437976 422000))
+;;; Generated autoloads from mparen.el
+
+(autoload 'jump-to-matching-paren "mparen" "\
+Go to the matching parenthesis if on parenthesis otherwise insert a hash.
+
+\(fn ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("andrew-email-mode.el" "andrew-ispell.el"
+;;;;;;  "andrew-notmuch.el" "andrew-org-capture.el" "andrew-org-setup.el"
+;;;;;;  "andrew-org-v2-setup.el" "andrew-org.el" "andrew-work-log.el"
+;;;;;;  "format-comment.el") (22329 40290 956157 906000))
+
+;;;***
+
+(provide 'andrew-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; andrew-autoloads.el ends here

3388ba4c4d6d680520ad4fec4e5df5c5088ea506
Andrew Burgess <andrew.burgess@embecosm.com>
2019-03-11 10:13:57 +0000

    emacs/notmuch: Correct quoting for hide-tags in sections list
    Was missing a quote on for the hide-tags attribute on the section list
    for the notmuch hello screen.

--- home/DOT_emacs.d/my-elisp/andrew-notmuch.el
+++ home/DOT_emacs.d/my-elisp/andrew-notmuch.el
@@ -174,79 +174,79 @@ If FILENAME is not passed then the file ~/.notmuch-searchs is loaded."
      "All tags (all mail)"
      :search-type tree
      :initially-hidden (not notmuch-show-all-tags-list)
-     :hide-tags notmuch-hello-hide-tags
+     :hide-tags 'notmuch-hello-hide-tags
      :filter notmuch-hello-tag-list-make-query)
     (notmuch-hello-insert-tags-section
      "All tags (all unread)"
      :search-type tree
      :initially-hidden (not notmuch-show-all-tags-list)
-     :hide-tags notmuch-hello-hide-tags
+     :hide-tags 'notmuch-hello-hide-tags
      :filter "tag:unread")
     (notmuch-hello-insert-tags-section
      "All tags (last hour/unread)"
      :search-type tree
      :initially-hidden (not notmuch-show-all-tags-list)
-     :hide-tags notmuch-hello-hide-tags
+     :hide-tags 'notmuch-hello-hide-tags
      :filter "date:1h.. AND tag:unread")
     (notmuch-hello-insert-tags-section
      "All tags (last 6hrs/unread)"
      :search-type tree
      :initially-hidden (not notmuch-show-all-tags-list)
-     :hide-tags notmuch-hello-hide-tags
+     :hide-tags 'notmuch-hello-hide-tags
      :filter "date:6h.. AND tag:unread")
     (notmuch-hello-insert-tags-section
      "All tags (last 12hrs/unread)"
      :search-type tree
      :initially-hidden (not notmuch-show-all-tags-list)
-     :hide-tags notmuch-hello-hide-tags
+     :hide-tags 'notmuch-hello-hide-tags
      :filter "date:12h.. AND tag:unread")
     (notmuch-hello-insert-tags-section
      "All tags (last 24hrs/unread)"
      :search-type tree
      :initially-hidden (not notmuch-show-all-tags-list)
-     :hide-tags notmuch-hello-hide-tags
+     :hide-tags 'notmuch-hello-hide-tags
      :filter "date:24h.. AND tag:unread")
     (notmuch-hello-insert-tags-section
      "All tags (last 7 days/unread)"
      :search-type tree
      :initially-hidden (not notmuch-show-all-tags-list)
-     :hide-tags notmuch-hello-hide-tags
+     :hide-tags 'notmuch-hello-hide-tags
      :filter "date:7d.. AND tag:unread")
     (notmuch-hello-insert-tags-section
      "All tags (14 -> 7 days ago/unread)"
      :search-type tree
      :initially-hidden (not notmuch-show-all-tags-list)
-     :hide-tags notmuch-hello-hide-tags
+     :hide-tags 'notmuch-hello-hide-tags
      :filter "date:14d..7d AND tag:unread")
     (notmuch-hello-insert-tags-section
      "All tags (21 -> 14 days ago/unread)"
      :search-type tree
      :initially-hidden (not notmuch-show-all-tags-list)
-     :hide-tags notmuch-hello-hide-tags
+     :hide-tags 'notmuch-hello-hide-tags
      :filter "date:21d..14d AND tag:unread")
     (notmuch-hello-insert-tags-section
      "All tags (last month/unread)"
      :search-type tree
      :initially-hidden (not notmuch-show-all-tags-list)
-     :hide-tags notmuch-hello-hide-tags
+     :hide-tags 'notmuch-hello-hide-tags
      :filter "date:1M.. AND tag:unread")
     (notmuch-hello-insert-tags-section
      "All tags (last 2 months/unread)"
      :search-type tree
      :initially-hidden (not notmuch-show-all-tags-list)
-     :hide-tags notmuch-hello-hide-tags
+     :hide-tags 'notmuch-hello-hide-tags
      :filter "date:2M.. AND tag:unread")
     (notmuch-hello-insert-tags-section
      "All tags (last 3 months/unread)"
      :search-type tree
      :initially-hidden (not notmuch-show-all-tags-list)
-     :hide-tags notmuch-hello-hide-tags
+     :hide-tags 'notmuch-hello-hide-tags
      :filter "date:3M.. AND tag:unread")
     (notmuch-hello-insert-tags-section
      "All tags (last 6 months/unread)"
      :search-type tree
      :initially-hidden (not notmuch-show-all-tags-list)
-     :hide-tags notmuch-hello-hide-tags
+     :hide-tags 'notmuch-hello-hide-tags
      :filter "date:6M.. AND tag:unread")
     notmuch-hello-insert-footer)
   "The value in this variable replaces `notmuch-hello-sections'
